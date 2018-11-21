6149d3de5819de1f24562bba1595f53db713571b
SEKINAT ALIU <sekinataliu1@Sekinats-Air.T-mobile.com>
2018-11-21 15:47:49 -0800

    Feat finalize ng bootstrap use for home page

--- movie-web-app/src/app/actors/actors.component.ts
+++ movie-web-app/src/app/actors/actors.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Actor } from './actor';
+import { ALL_ACTORS } from './real-actors';
 
 @Component({
   selector: 'app-actors',
@@ -9,7 +10,7 @@ import { Actor } from './actor';
 })
 export class ActorsComponent implements OnInit {
 
-  actors: Actor[];
+  actors = ALL_ACTORS;
 
   constructor() { }
 
