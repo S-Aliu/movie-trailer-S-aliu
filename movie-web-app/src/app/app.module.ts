8faef752becde16ea69b74854756f2bf80faef23
SEKINAT ALIU <sekinataliu1@Sekinats-Air.T-mobile.com>
2018-11-21 08:43:06 -0800

    Feat add actor compenent

--- movie-web-app/src/app/app.module.ts
+++ movie-web-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ActorsComponent } from './actors/actors.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ActorsComponent
   ],
   imports: [
     BrowserModule,
