c8d46d2dac18620778662164f1b3ef3404d65c3c
SEKINAT ALIU <sekinataliu1@Sekinats-MacBook-Air.local>
2018-11-28 09:55:45 -0800

    Feat add apostrophe

--- movie-web-app/src/app/actors/real-movies.ts
+++ movie-web-app/src/app/actors/real-movies.ts
@@ -4,14 +4,14 @@ export const THE_COLOR_PURPLE: Movie = {
   name: 'The Color Purple',
   trailerURL: 'https://www.imdb.com/title/tt0088939/videoplayer/vi2190868761?ref_=tt_ov_vi',
   path: 'assets/img/purple.jpg',
-  summary: 'An epic tale spanning forty years in the life of Celie (Whoopi Goldberg), an African-American woman living in the South who survives incredible abuse and bigotry. After Celie s abusive father marries her off to the equally debasing "Mister" Albert Johnson (Danny Glover), things go from bad to worse, leaving Celie to find companionship anywhere she can. She perseveres, holding on to her dream of one day being reunited with her sister in Africa. Based on the novel by Alice Walker.'
+  summary: 'An epic tale spanning forty years in the life of Celie (Whoopi Goldberg), an African-American woman living in the South who survives incredible abuse and bigotry. After Celie\'s abusive father marries her off to the equally debasing "Mister" Albert Johnson (Danny Glover), things go from bad to worse, leaving Celie to find companionship anywhere she can. She perseveres, holding on to her dream of one day being reunited with her sister in Africa. Based on the novel by Alice Walker.'
 };
 
 export const DO_THE_RIGHT_THING: Movie = {
   name: 'Do The Right Thing',
   trailerURL: 'https://www.imdb.com/title/tt0097216/videoplayer/vi3282174233?ref_=tt_ov_vi',
   path: 'assets/img/right.jpg',
-  summary: 'Salvatore "Sal" Fragione (Danny Aiello) is the Italian owner of a pizzeria in Brooklyn. A neighborhood local, Buggin Out (Giancarlo Esposito), becomes upset when he sees that the pizzerias Wall of Fame exhibits only Italian actors. Buggin Out believes a pizzeria in a black neighborhood should showcase black actors, but Sal disagrees. The wall becomes a symbol of racism and hate to Buggin Out and to other people in the neighborhood, and tensions rise.'
+  summary: 'Salvatore "Sal" Fragione (Danny Aiello) is the Italian owner of a pizzeria in Brooklyn. A neighborhood local, Buggin\' Out (Giancarlo Esposito), becomes upset when he sees that the pizzerias Wall of Fame exhibits only Italian actors. Buggin\' Out believes a pizzeria in a black neighborhood should showcase black actors, but Sal disagrees. The wall becomes a symbol of racism and hate to Buggin\' Out and to other people in the neighborhood, and tensions rise.'
 
 };
 
@@ -26,5 +26,12 @@ export const MALCOLM_X: Movie = {
   name: 'Malcolm X',
   trailerURL: 'https://www.imdb.com/title/tt0104797/videoplayer/vi3885236505?ref_=tt_ov_vi',
   path:'assets/img/malcolm.jpg',
-  summary:'A tribute to the controversial black activist and leader of the struggle for black liberation. He hit bottom during his imprisonment in the s50s, he became a Black Muslim and then a leader in the Nation of Islam. His assassination in 1965 left a legacy of self-determination and racial pride.'
+  summary:'A tribute to the controversial black activist and leader of the struggle for black liberation. He hit bottom during his imprisonment in the 50\'s, he became a Black Muslim and then a leader in the Nation of Islam. His assassination in 1965 left a legacy of self-determination and racial pride.'
+};
+
+export const GOOD_FENCES: Movie = {
+  name: 'Good Fences',
+  trailerURL: 'https://www.allmovie.com/movie/v278870',
+  path:'assets/img/fences.jpg',
+  summary:'Tom is an upwardly-mobile black middle-class attorney determined to \'end the colored man\'s losing streak\'. Now the family is off to the suburbs. It is 1973, and the lily-white community of Greenwich, Connecticut, doesn\'t quite deliver on the promise of the American Dream.'
 };
