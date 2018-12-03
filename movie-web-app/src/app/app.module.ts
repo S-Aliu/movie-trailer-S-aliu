579d88087f88aabc18a5531737bf9a3cf27012e4
SEKINAT ALIU <sekinataliu1@Sekinats-Air.T-mobile.com>
2018-12-02 16:19:01 -0800

    Feat import HttpClient and Forms modules

--- movie-web-app/src/app/app.module.ts
+++ movie-web-app/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ActorsComponent } from './actors/actors.component';
 import { ActorDetailComponent } from './actor-detail/actor-detail.component';
@@ -16,7 +18,9 @@ import { MovieService } from './movie.service';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [ MovieService ],
   bootstrap: [AppComponent]
